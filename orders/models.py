a12f50733d36ae651cfad0b19a2e8712b3c6a2c9
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-07-09 10:44:01 -0500

    models for order orderitem is added

--- orders/models.py
+++ orders/models.py
@@ -10,3 +10,21 @@ class Meal(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Order(models.Model):
+    customer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Order {self.id} by {self.customer.username}"
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    meal = models.ForeignKey(Meal, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.meal.name}"
+    
+
