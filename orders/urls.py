2fa5e24500bb53c4e4ffcc548b80462d00325678
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-07-19 05:04:57 -0500

    urls updated for update_item_status, barcode views, vendor details view, remove_cart, barcode scan, , order item status udpate

--- orders/urls.py
+++ orders/urls.py
@@ -6,22 +6,29 @@ from .views import (
     MealUpdateView,
     MealDeleteView,
     add_to_cart,
+    remove_from_cart,
     view_cart,
     checkout,
     invoice_detail_view,
     process_payment_view,
     update_order_item_status_view,
+    update_item_status,
     order_confirmation_view,
     order_history_view,
     view_item_barcode,
     confirm_pickup_view,
     VendorOrderHistoryView,
-    printable_barcode_view
+    printable_barcode_view,
+    print_bulk_barcodes_view,
+    qr_code_scanner_view,
+    scan_and_verify_view,
+    vendor_detail_view
 )
 
 urlpatterns = [
     path('', homepage_view, name='homepage'),
     path('add-to-cart/<int:meal_id>/', add_to_cart, name='add_to_cart'),
+    path('remove-from-cart/<str:cart_item_id>/', remove_from_cart, name='remove_from_cart'),
     path('cart/', view_cart, name='view_cart'),
     path('checkout/', checkout, name='checkout'),
     path('invoice/<int:order_id>/', invoice_detail_view, name='invoice_detail'),
@@ -30,13 +37,18 @@ urlpatterns = [
     path('order-history/', order_history_view, name='order_history'),
     path('order-item/<int:item_id>/barcode/', view_item_barcode, name='view_item_barcode'),
     path('order-item/<int:item_id>/confirm-pickup/', confirm_pickup_view, name='confirm_pickup'),
+    path('scan/', qr_code_scanner_view, name='scan_barcode'),
+    path('order-item/scan-verify/', scan_and_verify_view, name='scan_verify_barcode'),
 
     # Vendor views
+    path('vendor/<int:vendor_id>/', vendor_detail_view, name='vendor_detail'),
     path('dashboard/', VendorDashboardView.as_view(), name='vendor_dashboard'),
     path('dashboard/meal/add/', MealCreateView.as_view(), name='meal_add'),
     path('dashboard/meal/<int:pk>/edit/', MealUpdateView.as_view(), name='meal_edit'),
     path('dashboard/meal/<int:pk>/delete/', MealDeleteView.as_view(), name='meal_delete'),
     path('order-item/<int:item_id>/update-status/', update_order_item_status_view, name='update_order_item_status'),
+    path('order-item/<int:item_id>/update/<str:new_status>/', update_item_status, name='update_item_status'),
     path('dashboard/history/', VendorOrderHistoryView.as_view(), name='vendor_order_history'),
-    path('order-item/<int:item_id>/print/', printable_barcode_view, name='printable_barcode')
+    path('order-item/<int:item_id>/print/', printable_barcode_view, name='printable_barcode'),
+    path('dashboard/print-bulk-barcodes/', print_bulk_barcodes_view, name='print_bulk_barcodes'),
 ]
