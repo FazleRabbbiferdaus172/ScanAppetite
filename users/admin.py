da1773ec74f55214e029e246d1a43849571b11d7
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-08-06 02:16:08 -0500

    action to appriving vendors added to admin dashboard

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser, Profile
+from core.admin import custom_admin_site
 
 
 class ProfileInline(admin.StackedInline):
@@ -20,6 +21,15 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = UserAdmin.add_fieldsets + (
         (None, {'fields': ('user_type',)}),
     )
+    actions = ['approve_vendors']
+
+    def approve_vendors(self, request, queryset):
+        for user in queryset:
+            if user.user_type == 'VENDOR':
+                user.profile.is_approved = True
+                user.profile.save()
+        self.message_user(request, "Selected vendors have been approved.")
+    approve_vendors.short_description = "Approve selected vendors"
 
     def is_vendor_approved(self, obj):
         if obj.user_type == 'VENDOR':
@@ -28,5 +38,5 @@ class CustomUserAdmin(UserAdmin):
     is_vendor_approved.boolean = True # Shows a nice icon
     is_vendor_approved.short_description = 'Vendor Approved'
 
-# Register your CustomUser model with the custom admin class
-admin.site.register(CustomUser, CustomUserAdmin)
+
+custom_admin_site.register(CustomUser, CustomUserAdmin)
