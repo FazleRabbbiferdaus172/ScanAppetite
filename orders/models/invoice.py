6a10008f591b476140225df35ce9aa8307fcf5f6
FazleRabbbiferdaus172 <fazle.ferdaus1416@gmail.com>
2025-08-23 01:05:41 -0500

    price related changes

--- orders/models/invoice.py
+++ orders/models/invoice.py
@@ -8,6 +8,7 @@ class Invoice(models.Model):
         PAID = 'PAID', 'Paid'
 
     order = models.OneToOneField(Order, on_delete=models.CASCADE)
+    # Todo: Total amount should be paid amount and order_total_amount?
     total_amount = models.DecimalField(max_digits=10, decimal_places=2)
     status = models.CharField(max_length=10, choices=InvoiceStatus.choices, default=InvoiceStatus.UNPAID)
     issued_at = models.DateTimeField(auto_now_add=True)
